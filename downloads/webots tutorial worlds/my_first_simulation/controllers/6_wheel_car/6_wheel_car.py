508d53a8ba4b64c3e5e72cebc5901b5b573b7784
s40723210 <40723210@gm.nfu.edu.tw>
2020-01-10 14:43:01 +0800

    期末報告影片

---
+++ downloads/webots tutorial worlds/my_first_simulation/controllers/6_wheel_car/6_wheel_car.py
@@ -0,0 +1,22 @@
+from controller import Robot
+
+TIME_STEP = 64
+robot = Robot()
+# initialize motors
+wheels = []
+wheelsNames = ['wheel1', 'wheel2', 'wheel3', 'wheel4', 'wheel5', 'wheel6']
+for name in wheelsNames:
+    wheels.append(robot.getMotor(name))
+speed = -2.5 # [rad/s]
+wheels[0].setPosition(float('inf'))
+wheels[0].setVelocity(speed)
+wheels[1].setPosition(float('inf'))
+wheels[1].setVelocity(speed)
+wheels[2].setPosition(float('inf'))
+wheels[2].setVelocity(speed)
+wheels[3].setPosition(float('inf'))
+wheels[3].setVelocity(speed)
+wheels[4].setPosition(float('inf'))
+wheels[4].setVelocity(speed)
+wheels[5].setPosition(float('inf'))
+wheels[5].setVelocity(speed)
